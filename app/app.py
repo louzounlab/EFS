3200a0fa79f1a47a3e26c72d56e59ee302db51af
YuliTshuva <yuli.tshuva@gmail.com>
2024-03-14 15:04:01 +0200

    Update places of files app and requirements

--- app/app.py
+++ app/app.py
@@ -283,7 +283,7 @@ def calculate():
     # Save the df to a temporary file
     current_time = time.time()
     created_df_path = f"static/temp_files/{current_time}.csv"
-    df.to_csv(created_df_path, index=False)
+    df.to_csv(created_df_path, index=False) 
 
     # Predict on the data
     result = predict(created_df_path)
